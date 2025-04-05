d2300997fd94f4602f50ced15d30cdaef1d1b05e
Erik <slmv_e@icloud.com>
2025-04-05 22:30:41 +0300

    chore: created playground

---
+++ playground/index.tsx
@@ -0,0 +1,106 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Outlet } from 'react-router'
+
+import {
+  createLayoutView,
+  createRoute,
+  createRoutesView,
+  createRouteView,
+  createTSRouter,
+  TSLink,
+  TSRouterProvider,
+  useTSParams,
+} from '../src'
+
+const routes = {
+  home: createRoute(),
+  products: { feed: createRoute(), item: createRoute({ id: 'number' }) },
+}
+
+const router = createTSRouter({
+  routes: [
+    { path: '/', route: routes.home },
+    { path: '/products', route: routes.products.feed },
+    { path: '/products/:id', route: routes.products.item },
+  ],
+})
+
+const HomePage = () => {
+  return <div>Home</div>
+}
+
+const ProductsFeedPage = () => {
+  return <div>Products feed page</div>
+}
+
+const ProductItemPage = () => {
+  const { id } = useTSParams(routes.products.item)
+
+  return (
+    <div>
+      Product item page, id: {id}, typeof: {typeof id}
+    </div>
+  )
+}
+
+const HomePageView = createRouteView({
+  route: routes.home,
+  Component: HomePage,
+})
+
+const ProductsFeedPageView = createRouteView({
+  route: routes.products.feed,
+  Component: ProductsFeedPage,
+})
+
+const ProductItemPageView = createRouteView({
+  route: routes.products.item,
+  Component: ProductItemPage,
+})
+
+const RootLayout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <TSLink to={routes.home}>Home</TSLink>
+          </li>
+          <li>
+            <TSLink to={routes.products.feed}>Products</TSLink>
+          </li>
+          <li>
+            <TSLink to={routes.products.item} params={{ id: 1 }}>
+              Product
+            </TSLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  )
+}
+
+const RootLayoutView = createLayoutView({
+  Component: RootLayout,
+})
+
+const RoutesView = createRoutesView({
+  views: [
+    RootLayoutView(HomePageView, ProductsFeedPageView, ProductItemPageView),
+  ],
+})
+
+const root = createRoot(document.querySelector('#app')!)
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <TSRouterProvider router={router}>
+        <RoutesView />
+      </TSRouterProvider>
+    </BrowserRouter>
+  )
+}
+
+root.render(<App />)
